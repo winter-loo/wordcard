4a0437615adf36714ffeb536525c24d2ca55648f
winter.loo <ldd@deeloo.cn>
2023-04-18 05:28:31 +0800

    post literal with sentence and source

--- anykeeper/background.js
+++ anykeeper/background.js
@@ -9,13 +9,50 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   console.log('menu item clicked: ', info);
-  if (info.menuItemId == 'chrome.ext.ldd.cool') {
-    postText(info.selectionText);
-  }
+
+  const { id, url } = tab;
+  chrome.scripting.executeScript({
+    target: { tabId: id, allFrames: true },
+    func: () => {
+      // 获取选中文本
+      var selectedText = window.getSelection().getRangeAt(0).toString();
+
+      // 获取选中文本所在的最近公共祖先元素
+      var commonAncestor = window.getSelection().getRangeAt(0).commonAncestorContainer;
+
+      // 获取公共祖先元素的文本内容
+      var ancestorText = commonAncestor.textContent || commonAncestor.innerText;
+
+      // 查找包含所选单词的句子
+      var sentences = ancestorText.split(/[.!?]/g);
+      var selectedSentence = '';
+      for (var i = 0; i < sentences.length; i++) {
+        if (sentences[i].indexOf(selectedText) >= 0) {
+          selectedSentence = sentences[i];
+          break;
+        }
+      }
+
+      // 输出包含所选单词的句子
+      console.log(selectedSentence);
+      // TODO: 当前选中的句子并不一定是一个有效的句子。当选中的文本所在的句子含有
+      // 其他类型的节点（如，<a>, <pre>）时，`ancestorText` 并不包含完整的句子。
+      return selectedSentence.trim();
+    }
+  })
+    .then(injectionResults => {
+      sentence = injectionResults[0].result;
+      if (info.menuItemId == 'chrome.ext.ldd.cool') {
+        postText({
+          literal: info.selectionText,
+          sentence: sentence,
+          source: url
+        });
+      }
+    });
 });
 
-const postText = text => {
-  json_obj = { literal: text }
+const postText = json_obj => {
   console.log('post json: ', json_obj);
   (async () => {
     const response = await fetch('http://ldd.cool:1500/memo/add', {
